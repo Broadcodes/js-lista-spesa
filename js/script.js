d1e2b7facab1e49b05197536ba784640fbedd056
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-08-30 14:46:59 +0200

    Condizione per verificare la fine del ciclo

--- js/script.js
+++ js/script.js
@@ -6,6 +6,23 @@ Consigli:
 - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
 */
 
-// Creazione di un array per la lista della spesa
+const VALORE_SENTINELLA = "fine";
 
-// 
+// Creazione di un array per la lista della spesa
+const shoppingList = [];
+
+// Acquisizione dell'elemento input HTML di tipo button per gestire l'inserimento dei prodotti nella lista
+const buttonProduct = document.getElementById("buttonProduct");
+
+// Aggiungiamo evento al pulsante
+buttonProduct.addEventListener("click", function () {
+    // Acquisizione dell'elemento input HTML per l'inserimento dei prodotti
+    const product = document.getElementById("productName");
+
+    // Condizione per verificare se l'elemento inserito nell'input è una stringa (diversa dal valore sentinella "Fine" oppure un numero)
+    if (product.value.toLowerCase() !== VALORE_SENTINELLA && isNaN(product.value)) {
+        console.log("funziona");
+    } else {
+        console.log("Non funziona");
+    }
+});
