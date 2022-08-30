fbcaceca9f7cdfa19a801c85844c0d2f2eac1739
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-08-30 14:15:08 +0200

    Aggiunta di una nuova sezione grafica

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+Consegna:
+Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo while.
+Consigli:
+- Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+- Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+*/
+
+// Creazione di un array per la lista della spesa
+
+// 
